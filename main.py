bdc7915e17bd342cd88ee763326636f7b39544f2
JhonatanAlva <jloola@miumg.edu.gt>
2023-07-30 11:33:19 -0600

    Tercera version del codigo

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 if __name__ == '__main__':
     print('Hola Sistemas operativos')
     print("Esta es la segunda version del codigo")
+
+    a = 2;
+    b = 5;
+
+    suma = a + b;
+
+    print("El resultado de la suma es: ", suma)
